5a0aaba2b66d7eaa4d94ceb63a90b2cd78807950
Brendan Gannon <bgannon@bu.edu>
2013-03-22 14:34:42 +0000

    Fix syntax on jQuery patch

--- interface/js/bu-sequence-patch.js
+++ interface/js/bu-sequence-patch.js
@@ -1,7 +1,8 @@
 /* patching jQuery for sequence.js compatability with jQuery 1.4 */
 
 (function($) {
-	if (typeof $.on !== 'function') {
+	if (typeof $.fn.on !== 'function') {
+		console.log('no jQuery on defined');
 		$.fn.on = $.fn.bind;
 	}
 })(jQuery);
