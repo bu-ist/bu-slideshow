4335e8fbda1aee27958a1d11bca935e4e74b4a51
Inderpreet Singh <isingh@bu.edu>
2017-04-07 17:58:14 -0400

    Bump version to 2.3.9

--- bu-slideshow.php
+++ bu-slideshow.php
@@ -3,14 +3,14 @@
 Plugin Name: BU Slideshow
 Plugin URI: http://developer.bu.edu/bu-slideshow/
 Description: Allows for the creation and display of animated slideshows. Uses sequence.js.
-Version: 2.3.8
+Version: 2.3.9
 Author: Boston University (IS&T)
 Author URI: http://www.bu.edu/tech/
 Requires at least: 3.5
 Tested up to: 4.6.1
 */
 
-define('BU_SLIDESHOW_VERSION', '2.3.8');
+define('BU_SLIDESHOW_VERSION', '2.3.9');
 define('BU_SLIDESHOW_BASEDIR', plugin_dir_path(__FILE__));
 define('BU_SLIDESHOW_BASEURL', plugin_dir_url(__FILE__));
 // define('SCRIPT_DEBUG', true);
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: inderpreet99, awbauer, clrux, mgburns, gannondigital
 Tags: slideshow, images, boston university, bu
 Requires at least: 3.5
 Tested up to: 4.6.4
-Stable tag: 2.3.8
+Stable tag: 2.3.9
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
