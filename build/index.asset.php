814e0fb9786f33c82c1fb3b5ef3af33fa1eec7a5
Jonathan Williams <tamlin@mac.com>
2022-09-20 17:59:16 -0400

    commit updated build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e602dd8b882a2ae8c5e0594b08e4115');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ff280102fc105334677aa36d2b72f260');
