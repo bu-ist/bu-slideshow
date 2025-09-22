8285eaef2463ac52ca9a2a1dc58bef3faf17f82e
Jonathan Williams <tamlin@mac.com>
2025-09-22 15:57:37 -0400

    :package: commit js build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ff280102fc105334677aa36d2b72f260');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f7dac95a06eb231051e7');
